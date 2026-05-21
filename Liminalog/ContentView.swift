93305dcb8e3d4426389cc4d55008eb556a9ee3c2
Ryuga Yasuda <yuhapenguin@gmail.com>
2026-05-21 14:29:28 +0900

    Initial Commit

---
+++ Liminalog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Liminalog
+//
+//  Created by Ryuga on 2026/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Liminalog/LiminalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiminalogApp.swift
+//  Liminalog
+//
+//  Created by Ryuga on 2026/05/21.
+//
+
+import SwiftUI
+
+@main
+struct LiminalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
